656061b54c3ace08868322fabf73204451ea0414
Dmitriy1808 <dmitrij.99@yandex.ru>
2018-06-23 04:08:08 +0700

    Add generation.c and generation.h functions

---
+++ generation.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include "generation.h"
+
+void generation(int A[N],int B[N])
+{ 
+	srand(time(0));//rand
+	for (int i=0; i<N; i++)
+	{
+		bool f;
+		int temp;
+		do
+		{
+            f = 1;
+            temp = rand()%9;
+            for (int j = 0; j < i; j++) 
+			{
+                if (A[j] == temp)
+				{
+                    f = 0;
+                    break;
+				}
+			} 
+		} while(f==0);
+		A[i] = temp;
+	}
+}
+
