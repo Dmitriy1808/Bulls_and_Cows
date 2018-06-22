35bc47f4f2291fd88f8864aa8a4da3c01996fa95
Dmitriy1808 <dmitrij.99@yandex.ru>
2018-06-23 04:12:01 +0700

    Add game.c and game.h functions

---
+++ game.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "game.h"
+
+void game()
+{
+	int num=0;
+    generation(int A[N]);
+    printf("NUMBER B C\n");
+    printf("----------\n");
+    while(1)
+    {	
+        input(int B[N]);
+        num++;
+        int bull=bulls(int A[N], int B[N]),cor=cows(int A[N], int B[N]);
+        
+		if (num == 5 )
+			break;
+        printf("       %d %d \t\t\tAttempt &d\n",bull, cor, num);
+        if (b() == 4)
+        {
+            printf("YOU'RE WINS!!!"<<"\t\tQUANTITY OF ATTEMPTS=",num;
+            break;
+        }
+    }
+}
