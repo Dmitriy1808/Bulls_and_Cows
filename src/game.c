e9d1a23201bcee3a236b89fe21930882f6f2ca43
Dmitriy1808 <dmitrij.99@yandex.ru>
2018-06-23 05:50:21 +0700

    Fix game.c func

--- src/game.c
+++ src/game.c
@@ -4,21 +4,21 @@
 void game()
 {
 	int num=0;
-    generation(int A[N]);
+    generation(int A[N], int B[N]);
     printf("NUMBER B C\n");
     printf("----------\n");
     while(1)
     {	
         input(int B[N]);
         num++;
-        int bull=bulls(int A[N], int B[N]),cor=cows(int A[N], int B[N]);
+        int bull=bulls(int A[N], int B[N]), int cor = cows(int A[N], int B[N]);
         
 		if (num == 5 )
 			break;
         printf("       %d %d \t\t\tAttempt &d\n",bull, cor, num);
-        if (b() == 4)
+        if (bull() == 4)
         {
-            printf("YOU'RE WINS!!!"<<"\t\tQUANTITY OF ATTEMPTS=",num;
+            printf("YOU'RE WINS!!!"<<"\t\tQUANTITY OF ATTEMPTS=",num);
             break;
         }
     }
