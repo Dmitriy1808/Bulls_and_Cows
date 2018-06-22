819de7ff3b94dd617f6a71d83b146d7289b572de
Dmitriy1808 <dmitrij.99@yandex.ru>
2018-06-23 05:23:34 +0700

    Fix generation.c func

--- src/generation.c
+++ src/generation.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 #include "generation.h"
 
 void generation(int A[N],int B[N])
-{ 
-	srand(time(0));//rand
+{
+	srand(time(NULL));
 	for (int i=0; i<N; i++)
 	{
 		int f;
@@ -12,7 +13,7 @@ void generation(int A[N],int B[N])
 		do
 		{
             f = 1;
-            temp = rand()%9;
+            temp = rand() % 9;
             for (int j = 0; j < i; j++) 
 			{
                 if (A[j] == temp)
