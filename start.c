183a0beb80379939ac4dcfa5ef7b923bca75b306
kirill1sus <k.sus12@mail.ru>
2018-06-23 03:55:55 +0700

    Add func start.c and start.h

---
+++ start.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include "start.h"
+#include "game.h"
+
+int start (int A[N],int B[N])
+{
+    int f=0;
+    int M=0;
+    print("Hi this is a game of bulls and cows.The computer conceived the number you need to guess it");
+    while (f==0)
+    {
+        game();
+        print("want to play again?(YES=1/NO=0)\n");
+        scanf("%d",M);
+        if (M!=1)
+        {
+            f=1;
+        }
+        else
+        {
+            system(""cls);
+        }
+    }
+    system("pause");
+}
---
+++ start.h
@@ -0,0 +1,4 @@
+#pragma once 
+
+int start(int A[N],int B[n]);
+
