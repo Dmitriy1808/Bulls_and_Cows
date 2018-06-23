4ab8b1dbd85beabe6997d6b93a410f634dfe18bf
kirill1sus <k.sus12@mail.ru>
2018-06-23 21:52:21 +0700

    Added menu

--- src/start.c
+++ src/start.c
@@ -8,20 +8,31 @@ int main()
     int A[N], B[N];
     int f = 0;
     int M = 0;
-    printf("Hi this is a game of bulls and cows.The computer conceived the number you need to guess it");
-    while (f==0)
+    int S = 0;
+    printf("Hi this is a game of bulls and cows.The computer conceived the number you need to guess it\n");
+    printf("Game start=1 Exit=0 \n");
+       scanf("%d", &S);
+    if (S==1) 
     {
-        game(A,B);
-        printf("want to play again?(YES=1/NO=0)\n");
-        scanf("%d", &M);
-        if (M!=1)
+        while (f==0)
         {
-            f=1;
+            game(A,B);
+            printf("want to play again?(YES=1/NO=0)\n");
+            scanf("%d", &M);
+            if (M!=1)
+            {
+                f=1;
+            }
+            else
+            {
+                system("clear");
+            }
         }
-        else
-        {
-            system("clear");
-        }
+            system("pause");
+            
     }
-    system("pause");
+    else 
+    {
+        printf("Good bye \n");
+    }
 }
